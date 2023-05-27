f33b93213d8c0acff6c2dc036a3f0d281dcca8c6
hbohlen <bohlenhayden@gmail.com>
2023-05-26 22:02:19 -0500

    Base list item component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 import Header from "./components/Header";
+import ListItem from "./components/ListItem";
 
 import "./styles/global.css";
 
 const App: React.FC = () => {
+  const listItemData = {
+    name: "Item 1",
+    quantity: 1,
+  };
+
   return (
     <>
       <Header />
+      <ListItem name={listItemData.name} quantity={listItemData.quantity} />
     </>
   );
 };
