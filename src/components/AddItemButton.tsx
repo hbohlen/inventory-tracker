a0851770c850bfae74a80d9454cacc35544f7275
hbohlen <bohlenhayden@gmail.com>
2023-06-03 10:25:48 -0500

    Fixed add item button to bottom corner

--- src/components/AddItemButton.tsx
+++ src/components/AddItemButton.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const AddItemButton: React.FC = () => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded fixed bottom-4 right-4 z-10"
+      style={{ zIndex: 10 }}
+    >
       Add New Item
     </button>
   );
