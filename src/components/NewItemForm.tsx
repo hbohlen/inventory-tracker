92ed8bab6cc2509f956f39366dac242f2bad4cf9
hbohlen <bohlenhayden@gmail.com>
2023-06-03 10:52:30 -0500

    Added some styling to add item form

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -10,28 +10,42 @@ const NewItemForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
+    <form onSubmit={handleSubmit} className="p-4 bg-gray-200 rounded">
+      <div className="mb-4">
+        <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+          Name
+        </label>
         <input
           type="text"
           id="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
+          className="border border-gray-300 rounded px-3 py-2 w-full"
         />
       </div>
-      <div>
-        <label htmlFor="quantity">Quantity</label>
+      <div className="mb-4">
+        <label
+          htmlFor="quantity"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Quantity
+        </label>
         <input
           type="number"
           id="quantity"
           value={quantity}
           onChange={(e) => setQuantity(Number(e.target.value))}
           required
+          className="border border-gray-300 rounded px-3 py-2 w-full"
         />
       </div>
-      <button type="submit">Add Item</button>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+      >
+        Add Item
+      </button>
     </form>
   );
 };
