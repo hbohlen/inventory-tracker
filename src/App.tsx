8727338401b810471436ec4778bba9e3bd7c7d39
hbohlen <bohlenhayden@gmail.com>
2023-06-21 23:47:21 -0500

    Removed previous structure for refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import Header from "./components/Header";
+import Header from './components/Header';
 
-import ItemList from "./components/ItemList";
 
-import AddItemButton from "./components/AddItemButton";
 
-import NewItemForm from "./components/NewItemForm";
-
-import { listData } from "./data/listData";
-
-import "./styles/global.css";
+import './styles/global.css';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <ItemList items={listData} />
-
-      <AddItemButton />
+      
     </>
   );
 };
