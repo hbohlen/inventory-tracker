2013c344ccfbe9c05c39c3b22bc0999c24bae5b7
hbohlen <bohlenhayden@gmail.com>
2023-06-22 00:24:41 -0500

    Refactor application to simplify ItemList architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
+import { itemData } from './data/itemData';
 
+import { ItemList } from './components/ItemList/ItemList';
 
 import './styles/global.css';
 
 const App: React.FC = () => {
+  const [items, setItems] = useState<Array<Item>>(itemData);
   return (
     <>
       <Header />
-      
+
+      <ItemList items={items} />
     </>
   );
 };
--- src/data/listData.ts
+++
@@ -1,5 +0,0 @@
-export const listData = [
-  { name: "Item 1", quantity: 5 },
-  { name: "Item 2", quantity: 10 },
-  { name: "Item 3", quantity: 15 },
-];
