99c2e7d3daf8df123602240c5f9f403b8d93f021
hbohlen <bohlenhayden@gmail.com>
2023-06-23 01:17:23 -0500

    Setup Item loop for data and installed uuid for random key generations

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,62 @@ import Header from './components/Header';
 
 import { Item } from './components/Item';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import './styles/global.css';
 
 const App: React.FC = () => {
   const showItems = true;
+
+  const [items, setItems] = useState([
+    {
+      name: 'Item 1',
+      description: 'This is item 1',
+      quantity: 1,
+    },
+    {
+      name: 'Item 2',
+      description: 'This is item 2',
+      quantity: 2,
+    },
+    {
+      name: 'Item 3',
+      description: 'This is item 3',
+      quantity: 3,
+    },
+    {
+      name: 'Item 4',
+      description: 'This is item 4',
+      quantity: 4,
+    },
+    {
+      name: 'Item 5',
+      description: 'This is item 5',
+      quantity: 5,
+    },
+    {
+      name: 'Item 6',
+      description: 'This is item 6',
+      quantity: 6,
+    },
+  ]);
 
   return (
     <>
       <Header />
       {showItems ? (
         <>
-          <div className="flex flex-wrap">
-            <Item name={'Item 1'} quantity={5} />
-            <Item name={'Item 2'} quantity={10} />
-            <Item name={'Item 1'} quantity={5} />
-            <Item name={'Item 2'} quantity={10} />
-            <Item name={'Item 1'} quantity={5} />
-            <Item name={'Item 2'} quantity={10} />
-            <Item name={'Item 1'} quantity={5} />
-            <Item name={'Item 2'} quantity={10} />
-            <Item name={'Item 1'} quantity={5} />
-            <Item name={'Item 2'} quantity={10} />
+          <div className="flex flex-wrap justify-center">
+            {items.map((item) => {
+              return (
+                <Item
+                  key={uuidv4()}
+                  name={item.name}
+                  description={item.description}
+                  quantity={item.quantity}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
